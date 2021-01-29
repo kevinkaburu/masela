e42a107f8bac954cfb93898b6c3e50699cb157a9
Kaburu <mbayakelvin@gmail.com>
2021-01-29 17:03:26 +0300

    some blog vybes

---
+++ app/Http/Controllers/PrivacyController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * Description of BlogController
+ *
+ * @author root
+ */
+class PrivacyController extends Controller {
+    //put your code here
+    
+    
+    
+        public function index() {
+        return view('privacy.index');
+    }
+}
