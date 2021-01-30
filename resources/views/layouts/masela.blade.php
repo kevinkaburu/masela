b8f012417234d89f774e0a1c9b8318a7b39bdf5e
Kaburu <mbayakelvin@gmail.com>
2021-01-30 15:31:07 +0300

    add tawk chat

--- resources/views/layouts/masela.blade.php
+++ resources/views/layouts/masela.blade.php
@@ -10,6 +10,20 @@
 
   gtag('config', 'G-SDQ0G14XX3');
 </script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6015508ec31c9117cb7418cb/1et9k5bm8';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
