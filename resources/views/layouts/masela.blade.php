7a1351ee892725488239a7b13bfd86d59be98d57
Kaburu <mbayakelvin@gmail.com>
2021-02-20 12:57:17 +0300

    added facebook pixel

--- resources/views/layouts/masela.blade.php
+++ resources/views/layouts/masela.blade.php
@@ -23,7 +23,11 @@ s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
 </script>
+
 <!--End of Tawk.to Script-->
+<!-- Facebook Pixel Code -->
+<script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js'); fbq('init', '438060344102421'); fbq('track', 'PageView');</script><noscript> <img height="1" width="1" src="https://www.facebook.com/tr?id=438060344102421&ev=PageView&noscript=1"/></noscript>
+<!-- End Facebook Pixel Code -->
 <script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=601e98c2502e4d0011f5f23b&product=sop' async='async'></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
